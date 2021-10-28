502fb4cdd794b2322baef1fb43be205b0e60772e
Hitesh <garg98hitesh@gmail.com>
2021-10-28 16:20:19 +0530

    Update A Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -12,10 +12,32 @@ exports.createProduct = async(req,res,next)=>{
 //Get All Product
 exports.getAllProducts =async(req,res)=>{
 
-    //const products= await Product.find()
-     res.status(200).json( {message:"Route is working fine"})
-    // res.status(200).json({
-    //     success:true,
-    //     products
-   // })
+    const products= await Product.find()
+    //  res.status(200).json( {message:"Route is working fine"})
+    res.status(200).json({
+        success:true, 
+        products
+   })
+}
+
+//Update Product-- Admin
+
+exports.updatePorduct= async(req,res,next)=>{
+    let product = await Product.findById(req.param.id);
+    if(!product)
+    {
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+    product= await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false,   
+    })
+    res.status(200).json({
+        success:true,
+        product
+    })
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,7 +2,7 @@ const express=require("express");
 
 //const {getAllProducts}=require("../controllers/productController");
 
-const {getAllProducts,createProduct}=require("../controllers/productController");
+const {getAllProducts,createProduct, updatePorduct}=require("../controllers/productController");
 
 const router=express.Router();
 
@@ -10,4 +10,6 @@ const router=express.Router();
 router.route("/products").get(getAllProducts);
 
 router.route("/product/new").post(createProduct);
+
+router.route("/product/:id").put(updatePorduct)
 module.exports=router
